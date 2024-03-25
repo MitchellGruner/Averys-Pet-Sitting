0e273285fa77981c4689c48f8a81ecfabccf9577
Mitchell Gruner <mit.gruner@gmail.com>
2024-03-24 20:05:05 -0500

    initial commit of /components/navbar/NavbarMenu...

---
+++ app/components/navbar/NavbarMenuItems.tsx
@@ -0,0 +1,60 @@
+import Image from "next/image";
+
+const NavbarMenuItems = () => {
+    return (
+        <div className="flex flex-row justify-between rounded-full bg-white shadow-2xl border px-4 py-1">
+            <div className="flex flex-row justify-center px-4 py-2 cursor-pointer hover:text-teal transition ease-in-out duration-500 icon-container">
+                <Image
+                    src="/dogicon1.png"
+                    width={24}
+                    height={24}
+                    alt="Dog Icon"
+                />
+                <Image
+                    src="/dogiconhover1.png"
+                    width={24}
+                    height={24}
+                    alt="Dog Icon"
+                    className="icon-hover"
+                />
+                <span className="pl-2">Dog</span>
+            </div>
+            <div 
+                className="flex flex-row justify-center relative px-4 py-2 cursor-pointer border-l border-r hover:text-teal transition ease-in-out duration-500 icon-container"
+            >
+                <Image
+                    src="/mailicon.png"
+                    width={24}
+                    height={24}
+                    alt="Mail Icon"
+                />
+                <Image
+                    src="/mailiconhover.png"
+                    width={24}
+                    height={24}
+                    alt="Mail Icon"
+                    className="icon-hover"
+                />
+                <span className="pl-2">Mail</span>
+            </div>
+            <div className="flex flex-row justify-center px-4 py-2 cursor-pointer hover:text-teal transition ease-in-out duration-500 icon-container">
+                <Image
+                    src="/caticon.png"
+                    width={24}
+                    height={24}
+                    alt="Cat Icon"
+                />
+                <Image
+                    src="/caticonhover1.png"
+                    width={24}
+                    height={24}
+                    alt="Mail Icon"
+                    className="icon-hover"
+                />
+                <span className="pl-2">Cat</span>
+            </div>
+        </div>
+    ) 
+}
+
+export default NavbarMenuItems;
