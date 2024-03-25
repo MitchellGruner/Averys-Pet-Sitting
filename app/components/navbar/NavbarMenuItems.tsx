ba4986370d181aff2fcbedc1fe7ed09314bf9a54
Mitchell Gruner <mit.gruner@gmail.com>
2024-03-25 18:40:44 -0500

    improve main navigation pill with better styles

--- app/components/navbar/NavbarMenuItems.tsx
+++ app/components/navbar/NavbarMenuItems.tsx
@@ -1,58 +1,59 @@
+import Link from "next/link";
 import Image from "next/image";
 
 const NavbarMenuItems = () => {
     return (
-        <div className="flex flex-row justify-between rounded-full bg-white shadow-2xl border px-4 py-1">
-            <div className="flex flex-row justify-center relative px-4 py-2 cursor-pointer hover:text-dark-teal transition ease-in-out duration-500 icon-container">
-                <Image
-                    src="/dogicon1.png"
-                    width={24}
-                    height={24}
-                    alt="Dog Icon"
-                />
-                <Image
-                    src="/dogiconhover1.png"
-                    width={24}
-                    height={24}
-                    alt="Dog Icon"
-                    className="icon-hover absolute object-contain opacity-0 transition ease-in-out duration-500"
-                />
-                <span className="pl-2">Dog</span>
-            </div>
-            <div 
-                className="flex flex-row justify-center relative px-4 py-2 cursor-pointer border-l border-r hover:text-dark-teal transition ease-in-out duration-500 icon-container"
-            >
-                <Image
-                    src="/mailicon.png"
-                    width={24}
-                    height={24}
-                    alt="Mail Icon"
-                />
-                <Image
-                    src="/mailiconhover.png"
-                    width={24}
-                    height={24}
-                    alt="Mail Icon"
-                    className="icon-hover absolute object-contain opacity-0 transition ease-in-out duration-500"
-                />
-                <span className="pl-2">Contact Me</span>
-            </div>
-            <div className="flex flex-row justify-center relative px-4 py-2 cursor-pointer hover:text-dark-teal transition ease-in-out duration-500 icon-container">
-                <Image
-                    src="/caticon.png"
-                    width={24}
-                    height={24}
-                    alt="Cat Icon"
-                />
-                <Image
-                    src="/caticonhover1.png"
-                    width={24}
-                    height={24}
-                    alt="Mail Icon"
-                    className="icon-hover absolute object-contain opacity-0 transition ease-in-out duration-500"
-                />
-                <span className="pl-2">Cat</span>
-            </div>
+        <div className="hidden md:flex md:flex-row mx-auto">
+            <Link href="/dog" target="_blank" className="dog outline inline-block transition ease-in-out duration-500 icon-container">
+                <div className="flex flex-row items-center justify-center relative py-1 cursor-pointer hover:text-dark-teal">
+                    <Image
+                        src="/dogiconwhite2.png"
+                        width={24}
+                        height={24}
+                        alt="Dog Icon"
+                        className="unskew"
+                    />
+                    <Image
+                        src="/dogiconteal1.png"
+                        width={24}
+                        height={24}
+                        alt="Dog Icon"
+                        className="icon-hover icon-hover-first unskew absolute object-contain opacity-0 transition ease-in-out duration-500"
+                    />
+                    <span className="md:pl-2 lg:pl-4 font-bold unskew">Dog</span>
+                </div>
+            </Link>&nbsp;
+            <Link href="/contact" target="_blank" className="contact outline inline-block transition ease-in-out duration-500 icon-container">
+                <div className="flex flex-row items-center justify-center relative py-1 cursor-pointer">
+                    <Image
+                        src="/mailiconteal.png"
+                        width={24}
+                        height={24}
+                        alt="Mail Icon"
+                        className="unskew"
+                    />
+                    <span className="md:pl-2 lg:pl-4 font-bold unskew">Contact Me</span>
+                </div>
+            </Link>&nbsp;
+            <Link href="/cat" target="_blank" className="cat outline inline-block transition ease-in-out duration-500 icon-container">
+                <div className="flex flex-row items-center justify-center relative py-1 cursor-pointer hover:text-dark-teal">
+                    <Image
+                        src="/caticonwhite.png"
+                        width={24}
+                        height={24}
+                        alt="Cat Icon"
+                        className="unskew"
+                    />
+                    <Image
+                        src="/caticonteal.png"
+                        width={24}
+                        height={24}
+                        alt="Cat Icon"
+                        className="icon-hover icon-hover-third unskew absolute object-contain opacity-0 transition ease-in-out duration-500"
+                    />
+                    <span className="md:pl-2 lg:pl-4 font-bold unskew">Cat</span>
+                </div>
+            </Link>
         </div>
     )
 }
