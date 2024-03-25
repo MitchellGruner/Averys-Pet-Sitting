cd4398acdc7f4417713bb8dfbb823c8bec1898b8
Mitchell Gruner <mit.gruner@gmail.com>
2024-03-24 20:20:36 -0500

    add '/styles/Navbar.scss'

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+
+import "./styles/globals.css";
+import "./styles/Navbar.scss";
 
 import Navbar from "./components/navbar/Navbar";
 
