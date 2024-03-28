8326d53571ae008f7b30b4d8fcb69e83e29a092c
Mitchell Gruner <mit.gruner@gmail.com>
2024-03-28 11:51:28 -0500

    add responsive padding-y

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Main from "./components/main/Main";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen mx-auto max-w-[1600px] flex-col justify-between py-24">
+    <main className="flex min-h-screen mx-auto max-w-[1600px] flex-col justify-between lg:py-10 xl:py-24">
       <Main />
     </main>
   );
