903c4eeed1facadc1f794960ec9622d4e3694ae5
Mitchell Gruner <mit.gruner@gmail.com>
2024-04-01 11:33:59 -0500

    add 'scrollPosition' functionality for fixed image position

--- app/components/testimonials/Testimonials.tsx
+++ app/components/testimonials/Testimonials.tsx
@@ -1,7 +1,34 @@
+"use client";
+
+import React, { useState, useEffect } from 'react';
+import $ from 'jquery';
+
 const Testimonials = () => {
+    const [scrollPosition, setScrollPosition] = useState(0);
+    const handleScroll = () => {
+        const position = window.pageYOffset;
+        setScrollPosition(position);
+
+        if (position > 330) {
+            $("#mainBackground").css("position", "absolute");
+            $("#mainBackground").css("top", "540px");
+        } else {
+            $("#mainBackground").css("position", "fixed");
+            $("#mainBackground").css("top", "208px");
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll, { passive: true });
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
         <div className="block mt-[60em] mx-auto">
-            Testimonials
+            
         </div>
     )
 }
