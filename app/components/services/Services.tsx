497b2e4639dcaaf73753aaed0ebb7e659482e123
Mitchell Gruner <mit.gruner@gmail.com>
2024-04-14 14:11:44 -0500

    replace main services image, add margin

--- app/components/services/Services.tsx
+++ app/components/services/Services.tsx
@@ -6,18 +6,18 @@ import Image from "next/image";
 const Services = () => {
     return (
         <div id="services" className="block w-full mx-auto">
-            <div className="max-w-[1600px] testimonials-container mx-auto pt-10 pb-4">
+            <div className="max-w-[1200px] testimonials-container mx-auto pt-10 pb-4">
                 <Header title={"Services"} color={"text-white"} />
-                <div className="flex flex-row flex-wrap justify-center items-center">
-                    <div className="flex flex-col justify-center md:basis-1/2 pl-40">
+                <div className="mx-auto flex flex-row flex-wrap justify-center items-center">
+                    <div className="flex flex-col justify-center basis-full md:basis-2/3 pr-14">
                         <MuiAccordion />
                     </div>
-                    <div className="flex flex-row justify-center basis-full md:basis-1/2">
+                    <div className="flex flex-row justify-center basis-full md:basis-1/3">
                         <Image
-                            src="/averysaboutme.png"
-                            width={330}
-                            height={330}
-                            alt="Avery's About Me Picture"
+                            src="/servicescatimage.png"
+                            width={400}
+                            height={400}
+                            alt="Two Cats Services Image"
                             className="shadow-2xl"
                         />
                     </div>
