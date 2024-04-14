6363d19a5b5448d7de3c24c0bffe8734762230d3
Mitchell Gruner <mit.gruner@gmail.com>
2024-04-14 14:09:41 -0500

    add new accordion element, finish desktop tw styles

--- app/components/services/MuiAccordion.tsx
+++ app/components/services/MuiAccordion.tsx
@@ -19,53 +19,83 @@ export const MuiAccordion = () => {
             <Accordion
                 expanded={expanded === "panel1"}
                 onChange={(event, isExpanded) => handleChange(isExpanded, "panel1")}
-                className={`${expanded === 'panel1' ? 'rounded-bl-2xl rounded-br-2xl' : ''}`}
+                className={`${expanded === 'panel1' ? 'rounded-bl-2xl rounded-br-2xl' : ''} accordion-div-first`}
             >
                 <AccordionSummary
                     expandIcon={<ExpandMoreIcon />}
                     aria-controls="panel1-content"
                     id="panel1-header"
+                    className="shadow-2xl py-1 pl-8"
                 >
-                    <Typography className="flex flex-row text-lg">
+                    <Typography className="flex flex-row text-lg text-white font-bold">
                         <Image
-                            src="/services.panel1.image.png"
+                            src="/services.dog.bone.icon.png"
                             width={25}
                             height={25}
                             alt="Dog Bone Icon"
                             className="mr-4"
                         />
-                        Spending quality time with your furry friends
+                        <span className="pl-2">Spending quality time with your furry friends</span>
                     </Typography>
                 </AccordionSummary>
                 <AccordionDetails>
-                    <Typography className="text-md">
+                    <Typography className="text-md py-4 px-1">
                         Whether it's cuddling up on the couch, playing fetch in the backyard, or going for a walk around the neighborhood - I'm here to make sure your pets are happy and healthy while you're away.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion
+                expanded={expanded === "panel5"}
+                onChange={(event, isExpanded) => handleChange(isExpanded, "panel5")}
+                className={`${expanded === 'panel5' ? 'rounded-bl-2xl rounded-br-2xl' : ''} accordion-div-second`}
+            >
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel5-content"
+                    id="panel5-header"
+                    className="shadow-2xl py-1 pl-8"
+                >
+                    <Typography className="flex flex-row text-lg text-white font-bold">
+                        <Image
+                            src="/services.home.icon.png"
+                            width={25}
+                            height={25}
+                            alt="Home Icon"
+                            className="mr-4"
+                        />
+                        <span className="pl-2">Drop-in visits for personalized care</span>
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography className="text-md py-4 px-1">
+                        I provide personalized drop-in visits for your furry companions, ensuring they receive the love, attention, and care they need while you're away. From feeding and playtime to cuddles and companionship, I'll make sure your pets feel safe and happy until your return.
                     </Typography>
                 </AccordionDetails>
             </Accordion>
             <Accordion
                 expanded={expanded === "panel2"}
                 onChange={(event, isExpanded) => handleChange(isExpanded, "panel2")}
-                className={`${expanded === 'panel2' ? 'rounded-bl-2xl rounded-br-2xl' : ''}`}
+                className={`${expanded === 'panel2' ? 'rounded-bl-2xl rounded-br-2xl' : ''} accordion-div-third`}
             >
                 <AccordionSummary
                     expandIcon={<ExpandMoreIcon />}
                     aria-controls="panel2-content"
                     id="panel2-header"
+                    className="shadow-2xl py-1 pl-8"
                 >
-                    <Typography className="flex flex-row text-lg">
+                    <Typography className="flex flex-row text-lg text-white font-bold">
                         <Image
-                            src="/services.panel2.image.png"
+                            src="/services.pillow.icon.png"
                             width={25}
                             height={25}
                             alt="Pillow Icon"
                             className="mr-4"
                         />
-                        Overnight stays
+                        <span className="pl-2">Overnight stays</span>
                     </Typography>
                 </AccordionSummary>
                 <AccordionDetails>
-                    <Typography className="text-md">
+                    <Typography className="text-md py-4 px-1">
                         Overnight stays as part of my dog sitting service provide your furry companions with the comfort of familiar surroundings and consistent care throughout the night, ensuring they feel secure and loved even when you're away.
                     </Typography>
                 </AccordionDetails>
@@ -73,26 +103,27 @@ export const MuiAccordion = () => {
             <Accordion
                 expanded={expanded === "panel3"}
                 onChange={(event, isExpanded) => handleChange(isExpanded, "panel3")}
-                className={`${expanded === 'panel3' ? 'rounded-bl-2xl rounded-br-2xl' : ''}`}
+                className={`${expanded === 'panel3' ? 'rounded-bl-2xl rounded-br-2xl' : ''} accordion-div-fourth`}
             >
                 <AccordionSummary
                     expandIcon={<ExpandMoreIcon />}
                     aria-controls="panel3-content"
                     id="panel3-header"
+                    className="shadow-2xl py-1 pl-8"
                 >
-                    <Typography className="flex flex-row text-lg">
+                    <Typography className="flex flex-row text-lg text-white font-bold">
                         <Image
-                            src="/services.panel3.image1.png"
+                            src="/services.dumbbell.icon.png"
                             width={25}
                             height={25}
                             alt="Weights Icon"
                             className="mr-4"
                         />
-                        Playtime and exercise sessions
+                        <span className="pl-2">Playtime and exercise sessions</span>
                     </Typography>
                 </AccordionSummary>
                 <AccordionDetails>
-                    <Typography className="text-md">
+                    <Typography className="text-md py-4 px-1">
                         I prioritize regular exercise and play sessions tailored to your dog's needs, fostering physical health, mental stimulation, and a strong bond between us, ensuring they stay happy and healthy in your absence.
                     </Typography>
                 </AccordionDetails>
@@ -100,27 +131,28 @@ export const MuiAccordion = () => {
             <Accordion
                 expanded={expanded === "panel4"}
                 onChange={(event, isExpanded) => handleChange(isExpanded, "panel4")}
-                className={`${expanded === 'panel4' ? 'rounded-bl-2xl rounded-br-2xl' : ''}`}
+                className={`${expanded === 'panel4' ? 'rounded-bl-2xl rounded-br-2xl' : ''} accordion-div-fifth`}
             >
                 <AccordionSummary
                     expandIcon={<ExpandMoreIcon />}
                     aria-controls="panel4-content"
                     id="panel4-header"
+                    className="shadow-2xl py-1 pl-8"
                 >
-                    <Typography className="flex flex-row text-lg">
+                    <Typography className="flex flex-row text-lg text-white font-bold">
                         <Image
-                            src="/services.panel3.image.png"
+                            src="/services.car.icon.png"
                             width={25}
                             height={25}
-                            alt="Weights Icon"
+                            alt="Car Icon"
                             className="mr-4"
                         />
-                        Pet transportation services for vet visits or grooming appointments
+                        <span className="pl-2">Pet transportation services for appointments</span>
                     </Typography>
                 </AccordionSummary>
                 <AccordionDetails>
-                    <Typography className="text-md">
-                        I prioritize regular exercise and play sessions tailored to your dog's needs, fostering physical health, mental stimulation, and a strong bond between us, ensuring they stay happy and healthy in your absence.
+                    <Typography className="text-md py-4 px-1">
+                        I'll take the stress out of vet and grooming trips for your beloved pet with my personalized transportation service, ensuring they arrive safely and comfortably every time.
                     </Typography>
                 </AccordionDetails>
             </Accordion>
