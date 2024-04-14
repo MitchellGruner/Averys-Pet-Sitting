2b42e427055edfc71c145e2110b716918516de63
Mitchell Gruner <mit.gruner@gmail.com>
2024-04-14 17:24:23 -0500

    add tw styles for 'lg' screens and above

--- app/components/services/Services.tsx
+++ app/components/services/Services.tsx
@@ -6,13 +6,13 @@ import Image from "next/image";
 const Services = () => {
     return (
         <div id="services" className="block w-full mx-auto">
-            <div className="max-w-[1200px] testimonials-container mx-auto pt-10 pb-4">
+            <div className="max-w-[1200px] testimonials-container mx-auto xl:pt-10 xl:pb-4">
                 <Header title={"Services"} color={"text-white"} />
                 <div className="mx-auto flex flex-row flex-wrap justify-center items-center">
-                    <div className="flex flex-col justify-center basis-full md:basis-2/3 pr-14">
+                    <div className="flex flex-col justify-center basis-full md:basis-2/3 lg:pl-8 xl:pl-8 2xl:pl-0 xl:pr-10 2xl:pr-14">
                         <MuiAccordion />
                     </div>
-                    <div className="flex flex-row justify-center basis-full md:basis-1/3">
+                    <div className="flex flex-row justify-center basis-full md:basis-1/3 lg:pr-8 xl:pr-12 2xl:pr-0">
                         <Image
                             src="/servicescatimage.png"
                             width={400}
