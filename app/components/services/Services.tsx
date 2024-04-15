b394df28c68bb0d2b9986f47d064fcb57af2b0f0
Mitchell Gruner <mit.gruner@gmail.com>
2024-04-15 18:42:45 -0500

    finalize tw styling for mobile

--- app/components/services/Services.tsx
+++ app/components/services/Services.tsx
@@ -9,10 +9,10 @@ const Services = () => {
             <div className="max-w-[1200px] testimonials-container mx-auto xl:pt-10 xl:pb-4">
                 <Header title={"Services"} color={"text-white"} />
                 <div className="mx-auto flex flex-row flex-wrap justify-center items-center">
-                    <div className="flex flex-col justify-center basis-full md:basis-2/3 lg:pl-8 xl:pl-8 2xl:pl-0 xl:pr-10 2xl:pr-14">
+                    <div className="flex flex-col justify-center order-2 md:order-1 basis-full md:basis-2/3 md:pl-6 lg:pl-8 xl:pl-8 2xl:pl-0 xl:pr-10 2xl:pr-14">
                         <MuiAccordion />
                     </div>
-                    <div className="flex flex-row justify-center basis-full md:basis-1/3 lg:pr-8 xl:pr-12 2xl:pr-0">
+                    <div className="flex flex-row justify-center order-1 md:order-2 basis-full md:basis-1/3 md:pr-8 xl:pr-12 2xl:pr-0 py-6 xs:py-8 md:py-0">
                         <Image
                             src="/servicescatimage.png"
                             width={400}
