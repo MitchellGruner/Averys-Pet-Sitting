f71e3dea66786d55e17e7ae507828de2d83c7a78
Mitchell Gruner <mit.gruner@gmail.com>
2024-04-17 18:49:38 -0500

    remove <CalendlyForm /> declaration

--- app/components/contact/Contact.tsx
+++ app/components/contact/Contact.tsx
@@ -1,12 +1,12 @@
+"use client";
+
 import Header from "../Header";
-import CalendlyForm from "./CalendlyForm";
 
 const Contact = () => {
     return (
         <div id="contact" className="block w-full mx-auto 2xl:mt-16">
             <div className="max-w-[1200px] testimonials-container mx-auto xl:pt-10 xl:pb-4">
                 <Header title={"Contact"} color={"text-white"} />
-                <CalendlyForm url="https://calendly.com/mit-gruner" />
             </div>
         </div>
     )
