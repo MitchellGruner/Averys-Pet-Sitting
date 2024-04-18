fd3afa5e371f9a31cce339d39836f36da068e7d1
Mitchell Gruner <mit.gruner@gmail.com>
2024-04-18 11:28:52 -0500

    minimal clean up changes

--- app/components/contact/CalendlyForm.tsx
+++ app/components/contact/CalendlyForm.tsx
@@ -3,8 +3,6 @@
 import { PopupButton } from "react-calendly";
 import { useEffect, useState } from "react";
 
-import Image from "next/image";
-
 const CalendlyForm = () => {
   const [rootElement, setRootElement] = useState<HTMLElement | null>(null);
 
@@ -12,21 +10,19 @@ const CalendlyForm = () => {
     if (typeof window !== "undefined") {
       setRootElement(document.getElementById("__next"));
     }
-
-
   }, []);
 
   return (
     <div id="contactForm" className="fixed bottom-5 right-5 bg-light-brown rounded-full">
-      <div className="button-div">
+      <div className="sticky button-div">
         <PopupButton
-          className="sticky py-2 px-6 xl:py-3 xl:px-8 font-semibold rounded-full italic text-md xl:text-lg shadow-2xl text-white ease-in-out duration-500 hover:bg-black"
+          className="py-2 px-6 xl:py-3 xl:px-8 font-semibold rounded-full italic text-md xl:text-lg shadow-2xl text-white ease-in-out duration-500 hover:bg-black"
           url="https://calendly.com/mit-gruner"
           rootElement={rootElement!}
           text="Schedule Appointment"
         />
+        {/* <div className="button-div-absolute bg-white rounded-full absolute bottom-[-0.2em] right-1 py-3 px-28"></div> */}
       </div>
-      {/* <div className="button-div-absolute bg-white rounded-full absolute bottom-[-0.2em] right-1 py-3 px-28"></div> */}
     </div>
   );
 }
