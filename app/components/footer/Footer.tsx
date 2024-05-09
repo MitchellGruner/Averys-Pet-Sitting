4e8cf677fd8b3ba98b75ff417dc440366ba97563
Mitchell Gruner <mit.gruner@gmail.com>
2024-05-09 14:45:43 -0500

    finish responsive styles, add new elements for desktop

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -42,7 +42,7 @@ function FooterLink({
                 </Link>
             )}
             {linkFourth && nameFourth && (
-                <Link to={linkFourth} spy={true} smooth={true} duration={500} offset={-75} className="text-off-white text-base xl:text-lg cursor-pointer pb-1 hover:text-white transition ease-in-out duration-500">
+                <Link to={linkFourth} spy={true} smooth={true} duration={500} className="text-off-white text-base xl:text-lg cursor-pointer pb-1 hover:text-white transition ease-in-out duration-500">
                     {nameFourth}
                 </Link>
             )}
@@ -77,7 +77,30 @@ const Footer = () => {
         <div className="bg-slate w-full md:mt-40 lg:mt-48 xl:mt-52 py-4 border-t border-white z-10">
             <div className="max-w-[1600px] mx-auto flex flex-row justify-between">
                 <div className="flex flex-row justify-evenly basis-full md:basis-2/3">
-                    <div className="flex flex-col md:flex-row">
+                    <div className="hidden md:flex md:flex-row w-full justify-evenly">
+                        <FooterLink
+                            title="Home"
+                            linkFirst="about"
+                            nameFirst="About Me"
+                            linkSecond="services"
+                            nameSecond="Services"
+                            linkThird="contact"
+                            nameThird="Contact"
+                            linkFourth="contactForm"
+                            nameFourth="Get Connected"
+                        />
+                        <FooterLink
+                            title="Dog Services"
+                            linkFirst="dog"
+                            nameFirst="About"
+                        />
+                        <FooterLink
+                            title="Cat Services"
+                            linkFirst="cat"
+                            nameFirst="About"
+                        />
+                    </div>
+                    <div className="flex flex-col md:hidden">
                         <FooterLink
                             title="Home"
                             linkFirst="about"
@@ -95,13 +118,13 @@ const Footer = () => {
                             nameFirst="About"
                         />
                     </div>
-                    <div className="flex flex-col md:flex-row">
+                    <div className="flex flex-col md:hidden">
                         <FooterLink
                             title="Cat Services"
                             linkFirst="cat"
                             nameFirst="About"
                         />
-                        <a href="/" className="md:hidden md:min-w-44 flex flex-row justify-center mt-2">
+                        <Link to="home" spy={true} smooth={true} duration={500} offset={-200} className="flex flex-row justify-center mt-2 cursor-pointer">
                             <Image
                                 src="/averyspetsittinglogo1.png"
                                 width={150}
@@ -109,12 +132,12 @@ const Footer = () => {
                                 alt="Avery's Pet Sitting Logo"
                                 className="max-w-20 xs:max-w-28 mt-14"
                             />
-                        </a>
+                        </Link>
                     </div>
                 </div>
                 <div className="flex flex-row justify-evenly md:basis-1/3 md:pt-2 lg:pt-6">
                     <div className="flex flex-col">
-                        <a href="/" className="md:min-w-44 hidden md:flex flex-row justify-center mt-2">
+                        <Link to="home" spy={true} smooth={true} duration={500} offset={-200} className="md:min-w-44 hidden md:flex flex-row justify-center mt-2 cursor-pointer">
                             <Image
                                 src="/averyspetsittinglogo1.png"
                                 width={150}
@@ -122,7 +145,7 @@ const Footer = () => {
                                 alt="Avery's Pet Sitting Logo"
                                 className="max-w-16 md:max-w-full"
                             />
-                        </a>
+                        </Link>
                     </div>
                 </div>
             </div>
