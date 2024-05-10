00ee0b92a5a5481591f8ff5621595fc5fbf7d52a
Mitchell Gruner <mit.gruner@gmail.com>
2024-05-10 12:41:43 -0500

    initial commit of /app/HomePage.tsx

---
+++ app/HomePage.tsx
@@ -0,0 +1,27 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import AppContainer from "./components/AppContainer";
+import Main from "./components/main/Main";
+import About from "./components/about/About";
+import CalendlyForm from "./components/contact/CalendlyForm";
+import Contact from "./components/contact/Contact";
+import Services from "./components/services/Services";
+import Testimonials from "./components/testimonials/Testimonials";
+
+const HomePage = () => {
+    return (
+        <>
+            <Main />
+            <About />
+            <Testimonials />
+            <Services />
+            <ChakraProvider>
+                <AppContainer>
+                <Contact />
+                </AppContainer>
+            </ChakraProvider>
+            <CalendlyForm />
+        </>
+    )
+}
+
+export default HomePage;
