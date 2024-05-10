d56d7cec6b553526ed3301edcffefa24562d49aa
Mitchell Gruner <mit.gruner@gmail.com>
2024-05-10 12:42:01 -0500

    move home page components to dedicated component

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,10 @@
-import {ChakraProvider} from "@chakra-ui/react";
-import AppContainer from "./components/AppContainer";
-
-import Main from "./components/main/Main";
-import About from "./components/about/About";
-import Testimonials from "./components/testimonials/Testimonials";
-import Services from "./components/services/Services";
-import Contact from "./components/contact/Contact";
-import CalendlyForm from "./components/contact/CalendlyForm";
+import HomePage from "./HomePage";
 import Footer from "./components/footer/Footer";
 
 export default function Home() {
   return (
     <main className="flex mx-auto flex-col justify-between pt-8 lg:pt-10 xl:pt-24">
-      <Main />
-      <About />
-      <Testimonials />
-      <Services />
-      <ChakraProvider>
-        <AppContainer>
-          <Contact />
-        </AppContainer>
-      </ChakraProvider>
-      <CalendlyForm />
+      <HomePage />
       <Footer />
       <div id="__next"></div>
     </main>
