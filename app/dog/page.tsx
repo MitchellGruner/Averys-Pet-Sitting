68b978775896a8347d6528fa602afc6b0613afba
Mitchell Gruner <mit.gruner@gmail.com>
2024-05-17 18:46:50 -0500

    add responsive styling for main dog section

--- app/dog/page.tsx
+++ app/dog/page.tsx
@@ -3,25 +3,25 @@ import ContactButton from "../components/ContactButton";
 
 const Dog = () => {
     return (
-        <div id="dogPage" className="relative block w-full mx-auto mt-2 sm:mt-4 lg:mt-6 2xl:mt-8 pb-16 md:pb-40 lg:pb-48 xl:pb-52">
-            <div className="flex flex-col md:flex-row items-center justify-center mt-6 xs:mt-8 sm:mt-12 md:mt-20">
-                <div className="basis-full md:basis-1/2 pt-4 xs:pt-6 md:pt-0 md:pr-6 lg:pl-20 xl:pl-36 2xl:pl-56">
-                    <span className="absolute main-header block text-white 2xs:top-1 xs:top-6 sm:top-6 md:top-10 lg:top-0 md:left-0 lg:left-4">Paws and Play</span>
-                    <span className="absolute main-header main-header-secondary block text-white 2xs:top-6 xs:top-14 sm:top-16 md:top-20 lg:top-12 2xl:top-16 2xs:left-6 xs:left-6 sm:left-10 lg:left-24">
+        <div id="dogPage" className="relative block w-full mx-auto mt-2 sm:mt-4 lg:mt-6 2xl:mt-8 pb-16 md:pb-40 lg:pb-48 xl:pb-52 max-w-[1600px]">
+            <div className="flex flex-col md:flex-row items-center justify-center mt-6 xs:mt-8 sm:mt-12 md:mt-16 lg:mt-20 xl:mt-28 mx-6 xs:mx-8 sm:mx-12 md:mx-20 lg:mx-28 xl:mx-40 2xl:mx-72">
+                <div className="basis-full order-2 md:order-1 md:basis-1/2 pt-4 xs:pt-6 md:pt-0 md:pr-6">
+                    <span className="hidden sm:block absolute main-header text-white 2xs:top-1 xs:top-[12em] sm:top-[13.2em] md:top-0 md:left-0 lg:left-[1.2em] xl:left-[2em] 2xl:left-[4em]">Paws and Play</span>
+                    <span className="hidden sm:block absolute main-header main-header-secondary text-white 2xs:top-6 xs:top-[13em] sm:top-[14.2em] md:top-14 lg:top-12 xl:top-16 2xs:left-6 xs:left-6 sm:left-24 md:left-10 lg:left-[2.4em] xl:left-[3em] 2xl:left-[5.2em]">
                         Haven
                     </span>
 
-                    <p className="mt-32 2xs:text-sm sm:text-md xl:text-lg leading-6 text-white italic">
-                        Hey there! I'm Avery Geiger, and let me tell you, dog sitting isn't just a job for me — it's a passion. Whether it's a weekend getaway or an extended trip, I understand how tough it can be to leave your four-legged family member behind.
+                    <p className="sm:mt-32 md:mt-36 lg:mt-40 xl:mt-52 2xs:text-sm sm:text-base xl:text-lg leading-6 text-white italic">
+                        Hey there! I'm Avery Geiger, and let me tell you, dog sitting isn't just a job for me — <span className="text-yellow font-bold">it's a passion</span>. Whether it's a weekend getaway or an extended trip, I understand how tough it can be to leave your four-legged family member behind.
                     </p>
 
-                    <p className="mt-8 2xs:text-sm sm:text-md xl:text-lg leading-6 text-white italic">
-                        That's why I've made it my mission to provide a safe, loving, and fun-filled environment where your pup can thrive while you're away.
+                    <p className="mt-4 lg:mt-8 2xs:text-sm sm:text-base xl:text-lg leading-6 text-white italic">
+                        That's why I've made it my mission to provide a <span className="text-yellow font-bold">safe, loving, and fun-filled environment</span> where your pup can thrive while you're away.
                     </p>
 
                     <ContactButton text={"Get Started"} />
                 </div>
-                <div className="flex justify-start basis-full md:basis-1/2 md:pr-4 md:pl-8 lg:pl-12 px-2 xs:px-4 pb-6 xs:pb-8 md:pb-0 border-b-4 md:border-b-0 md:border-l-4 border-white">
+                <div className="flex justify-start basis-full order-1 md:order-2 md:basis-1/2 md:pr-4 md:pl-8 lg:pl-12 px-2 xs:px-4 pb-6 xs:pb-8 md:pb-0 border-b-4 md:border-b-0 md:border-l-4 border-white">
                     <Image
                         src="/dog.sitting.main.image.jpg"
                         width={400}
