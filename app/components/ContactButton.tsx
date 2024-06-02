3521c7415606f0987be21bd07e9518339b0fed4b
Mitchell Gruner <mit.gruner@gmail.com>
2024-06-02 14:18:37 -0500

    change hover color from light-brown to brown

--- app/components/ContactButton.tsx
+++ app/components/ContactButton.tsx
@@ -67,7 +67,7 @@ function handleClick() {
 
 const ContactButton = (props: Props) => {
     return (
-        <Link href="/#contactForm" onClick={handleClick} className={`inline-block mb-6 2xs:py-1 xs:py-2 sm:py-2 md:py-3 lg:py-2 2xs:px-4 sm:px-6 md:px-10 lg:px-14 2xs:mt-4 md:mt-6 lg:mt-10 text-center text-black text-md font-bold bg-white shadow-2xl uppercase hover:bg-light-brown transition ease-in-out duration-500 italic cursor-pointer ${props.class}`}>
+        <Link href="/#contactForm" onClick={handleClick} className={`inline-block mb-6 2xs:py-1 xs:py-2 sm:py-2 md:py-3 lg:py-2 2xs:px-4 sm:px-6 md:px-10 lg:px-14 2xs:mt-4 md:mt-6 lg:mt-10 text-center text-black text-md font-bold bg-white shadow-2xl uppercase hover:bg-brown transition ease-in-out duration-500 italic cursor-pointer ${props.class}`}>
             <div className="flex flex-row items-center justify-center">
                 <span className="2xs:contents xs:block pr-10 2xs:text-xs xs:text-sm lg:text-md 2xl:text-lg">{props.text}</span>
                 <svg fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
