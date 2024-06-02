09e4e62ba128eb9b8f9338aef3f8316461d56706
Mitchell Gruner <mit.gruner@gmail.com>
2024-06-02 14:17:59 -0500

    import "./styles/cat/Cat.scss"

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ import "./styles/Header.scss";
 import "./styles/dog/Dog.scss";
 import "./styles/dog/PhotoCallout.scss";
 import "./styles/dog/ImageBlurbs.scss";
+import "./styles/cat/Cat.scss";
 
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
