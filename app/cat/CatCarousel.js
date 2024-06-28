e46aec294843bce775048baab0e302ef6ab85a64
Mitchell Gruner <mit.gruner@gmail.com>
2024-06-28 17:25:20 -0500

    test - import image, video from "next"

--- app/cat/CatCarousel.js
+++ app/cat/CatCarousel.js
@@ -4,33 +4,21 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+import Image from "next/image";
+
+import Video from "./Video";
+
 const CatCarousel = () => {
     const settings = {
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 2000,
+        autoplaySpeed: 5000,
     };
 
     return (
-        <div id="catCarousel" className="w-3/4 m-auto">
-            <div className="mt-20">
-                <Slider {...settings}>
-                    {data.map((d) => (
-                        <div key={d.name} className="bg-white h-[450px] text-black rounded-xl">
-                            <div className="h-56 bg-indigo-500 flex justify-center items-center rounded-t-xl">
-                                <img src={d.img} alt={d.name} className="h-40" />
-                            </div>
-
-                            <div className="flex flex-col items-center justify-center gap-4 p-4">
-                                {d.paragraph}
-                            </div>
-                        </div>
-                    ))}
-                </Slider>
-            </div>
-        </div>
+        <div></div>
     )
 }
 
