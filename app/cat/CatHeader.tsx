080c74f3cce25f05dee7699b3cd5056bb7e8dcdf
Mitchell Gruner <mit.gruner@gmail.com>
2024-07-15 17:09:37 -0500

    make content div navy, highlighted text yellow

--- app/cat/CatHeader.tsx
+++ app/cat/CatHeader.tsx
@@ -27,8 +27,8 @@ const Header = () => {
                         />
                     </span>
                     
-                    <div className="bg-brown py-8 px-4 ml-[2.8em] xs:ml-[3.2em] sm:ml-[4.2em] md:ml-[8em] lg:ml-[9em] xl:ml-[10em] 2xl:ml-[12em] w-4/6 3xl:w-3/5 4xl:w-3/6 8xl:w-1/3 mt-[3.8em] xs:mt-[4.4em] sm:mt-[15.2em] md:mt-[16em] lg:mt-[18em] xl:mt-[20em] 8xl:ml-[24em] border border-white shadow-2xl">
-                        <p className="text-base sm:text-lg 2xl:text-xl pb-2 text-white pt-0 pl-0 lg:pl-4 italic">As a devoted cat sitter, <span className="text-black font-bold">my mission is simple:</span> to provide your furry friend with the utmost love, care, and attention while you're away. With a gentle touch and a heart full of affection for our feline friends, I ensure that each kitty under my care feels safe, happy, and cherished.</p>
+                    <div className="bg-navy py-8 px-4 ml-[2.8em] xs:ml-[3.2em] sm:ml-[4.2em] md:ml-[8em] lg:ml-[9em] xl:ml-[10em] 2xl:ml-[12em] w-4/6 3xl:w-3/5 4xl:w-3/6 8xl:w-1/3 mt-[3.8em] xs:mt-[4.4em] sm:mt-[15.2em] md:mt-[16em] lg:mt-[18em] xl:mt-[20em] 8xl:ml-[24em] border border-white shadow-2xl">
+                        <p className="text-base sm:text-lg 2xl:text-xl pb-2 text-white pt-0 pl-0 lg:pl-4 italic">As a devoted cat sitter, <span className="text-yellow font-bold">my mission is simple:</span> to provide your furry friend with the utmost love, care, and attention while you're away. With a gentle touch and a heart full of affection for our feline friends, I ensure that each kitty under my care feels safe, happy, and cherished.</p>
                     </div>
 
                     <ContactButton text={"Contact Me"} class={"xs:mt-[2.2em] sm:mt-[2.6em] md:mt-[2em] mb-[3.2em] xs:mb-[2.9em] sm:mb-[3.2em] md:mb-[0em] xs:ml-[6.6em] sm:ml-[13.2em] md:ml-[10em] lg:ml-[13.2em] xl:ml-[18.2em] 4xl:ml-[22.2em] 7xl:ml-[31em]"} />
