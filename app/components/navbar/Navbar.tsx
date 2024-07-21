af38b54a15b2e36d601876d17c1c1c3e53c414e2
Mitchell Gruner <mit.gruner@gmail.com>
2024-07-21 11:36:36 -0500

    add <header> tag, aria attributes for semantics

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -4,15 +4,21 @@ import Logo from "../Logo";
 
 const Navbar = () => {
     return (
-        <nav className="w-full fixed border-b border-white top-0 left-0 py-3 bg-navy z-10">
-            <div className="max-w-[1600px] mx-auto px-6">
-                <div className="flex flex-row justify-between items-center">
-                    <Logo className="max-w-16 md:max-w-full" width={90} height={90} />
-                    <NavbarMenuItems />
-                    <ProfileMenu />
+        <header>
+            <nav className="w-full fixed border-b border-white top-0 left-0 py-3 bg-navy z-10" role="navigation" aria-label="Main Navigation">
+                <div className="max-w-[1600px] mx-auto px-6">
+                    <div className="flex flex-row justify-between items-center">
+                        <Logo className="max-w-16 md:max-w-full" width={90} height={90} />
+                        <div role="menu" aria-label="Navbar Menu">
+                            <NavbarMenuItems />
+                        </div>
+                        <div role="menu" aria-label="Profile Menu">
+                            <ProfileMenu />
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        </header>
     )
 }
 
